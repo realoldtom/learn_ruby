cc0969ebfb3f4f39685a51c875e3ffe33df31815
J Eisenback <jeisenback@gmail.com>
2016-07-24 17:03:05 -0400

    updated all

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -6,6 +6,22 @@ def echo(word)
  return word.upcase
  end
  
- def repeat(*a)
-  a a
+ def repeat(word, n=2)
+  ( "#{word} " * n ).strip
+ end
+
+ def start_of_word(input, n)
+ 	input[0, n]
+ end
+
+ def first_word(input)
+ 	word = input.split.first
+ 	word
+ end
+
+ def titleize(input)
+ 	little_words = %w(end over and the)
+ 	input.capitalize.gsub(/(\w+)/) do |word|
+	little_words.include?(word) ? word : word.capitalize
+	end
  end
